8ce6d9104021f8cca40053b121ae66f36cbb2367
AARAV26102004 <Aravindmani8587@gmail.com>
2022-12-08 15:21:47 +0530

    JAVASCRIPT BASIC
    Javascript

---
+++ JS08122022/01.js
@@ -0,0 +1,9 @@
+var name="Aarav"
+console.log(name)
+
+function namenew(){
+    const name="Aravind";
+    console.log(name);
+}
+
+namenew()
---
+++ JS08122022/02.js
@@ -0,0 +1,11 @@
+let ak="Danger"
+alert(ak);
+
+let gokul="Gokul is a Playboy"
+alert(gokul)
+
+let harsha="Harshavarthan is a Talented boy"
+alert(harsha)
+
+let suriya="Suriya is such a Playboy u know"
+alert(suriya)
